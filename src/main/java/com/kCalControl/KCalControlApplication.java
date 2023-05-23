56cdc0beda1ac0d65ec9056c640f5b7e032bdd7d
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-05-23 14:02:44 +0200

    Rename main class

--- src/main/java/com/kCalControl/KCalControlApplication.java
+++ src/main/java/com/kCalControl/KCalControlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolApplication {
+public class KCalControlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToolApplication.class, args);
+		SpringApplication.run(KCalControlApplication.class, args);
 	}
 
 }
