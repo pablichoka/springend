521f6e46e930bb03341f1b28d174f92fba35fcd6
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-06-05 13:42:00 +0200

    Remove unusued search method

--- src/main/java/com/kCalControl/repository/UserRepository.java
+++ src/main/java/com/kCalControl/repository/UserRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserDB, Integer> {
-    public Optional<UserDB> findByUsername(String name);
-    public Optional<UserDB> findByFirstName(String name);
+    Optional<UserDB> findByUsername(String name);
 }
