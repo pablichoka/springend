7faa28e94c389dea24625382e7bceab8c0385086
Pablo Ruiz Abellán <pabloruiz16@hotmail.com>
2023-06-05 23:21:01 +0200

    Improved data edit, now ADMINs can edit everyone and USERs only themselves.

--- src/main/java/com/kCalControl/controller/UserController.java
+++ src/main/java/com/kCalControl/controller/UserController.java
@@ -2,7 +2,9 @@ package com.kCalControl.controller;
 
 import com.kCalControl.dto.UserDTO;
 import com.kCalControl.model.UserDB;
+import com.kCalControl.model.UserRole;
 import com.kCalControl.repository.UserRepository;
+import com.kCalControl.repository.UserRoleRepository;
 import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,9 +17,10 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RolesAllowed({"ADMIN","USER"})
+@RolesAllowed({"ADMIN", "USER"})
 @RequestMapping("/userActions")
 public class UserController {
 
@@ -26,9 +29,11 @@ public class UserController {
     BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    UserRoleRepository userRoleRepository;
 
     @GetMapping("/listUser")
-    private String listUser(Model model){
+    private String listUser(Model model) {
 
         UserDTO userDTO = new UserDTO();
 
@@ -40,7 +45,20 @@ public class UserController {
     }
 
     @GetMapping("/editUser/{id}")
-    public String editUser(@PathVariable("id") Integer id, Model model){
+    public String editUser(@PathVariable("id") Integer id, Model model, Principal principal) {
+
+        Optional<UserDB> optionalUserDB = userRepository.findByUsername(principal.getName());
+        if (optionalUserDB.isPresent()) {
+            UserDB currentUser = optionalUserDB.get();
+            Optional<UserRole> optionalUserRole = userRoleRepository.findById_UserDB_Id(currentUser.getId());
+
+            if (currentUser.getId() != id && optionalUserRole.isPresent() && !optionalUserRole.get().getRole().matches("ADMIN")) {
+                model.addAttribute("error", "You do not have permission to edit this user.");
+                return "/error/403";
+            }
+        }
+
+
         UserDB userDB = userRepository.findById(id).get();
 
         UserDTO userDTO = userDB.UserDB2UserDTO();
@@ -52,21 +70,31 @@ public class UserController {
 
         model.addAttribute("id", id);
         model.addAttribute("user", new UserDTO());
-        model.addAttribute("user2edit" , userDTO);
+        model.addAttribute("user2edit", userDTO);
 
         return "userActions/editUser";
     }
 
     @PostMapping("/updateUserData")
-    public String updateUserData(@ModelAttribute("user") UserDTO userDTO, @RequestParam("id") Integer id, Principal principal){
+    public String updateUserData(@ModelAttribute("user") UserDTO userDTO, @RequestParam("id") Integer id, Principal principal) {
 
         UserDB userDB = userRepository.findById(id).get();
         //Empty fields from the form are: String -> ""; Int, Double,...-> null
-        if(userDTO.getPassword() != ""){ userDB.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword())); }
-        if(userDTO.getEmail() != ""){ userDB.setEmail(userDTO.getEmail()); }
-        if(userDTO.getMobile() != ""){ userDB.setMobile(userDTO.getMobile()); }
-        if(userDTO.getAge() != null){ userDB.setAge(userDTO.getAge()); }
-        if(userDTO.getWeight() != null){ userDB.setWeight(userDTO.getWeight()); }
+        if (userDTO.getPassword() != "") {
+            userDB.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+        }
+        if (userDTO.getEmail() != "") {
+            userDB.setEmail(userDTO.getEmail());
+        }
+        if (userDTO.getMobile() != "") {
+            userDB.setMobile(userDTO.getMobile());
+        }
+        if (userDTO.getAge() != null) {
+            userDB.setAge(userDTO.getAge());
+        }
+        if (userDTO.getWeight() != null) {
+            userDB.setWeight(userDTO.getWeight());
+        }
 
         userDB.setModificationPerson(userRepository.findByUsername(principal.getName()).get());
         userDB.setModificationDate(LocalDateTime.now());
