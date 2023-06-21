950c915fc0a0b2ce343013e265f25111fd954459
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-06-21 10:07:48 +0200

    Fix profile view, pending lastName issue. Pending to create method updateUserData

--- src/main/java/com/kCalControl/controller/UserActionsController.java
+++ src/main/java/com/kCalControl/controller/UserActionsController.java
@@ -8,13 +8,11 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -67,5 +65,7 @@ public class UserActionsController {
 
         return "userActions/myProfile";
     }
+
+
 
 }
