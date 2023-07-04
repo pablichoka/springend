791d1f530c5907ba96936ed5afd6ac7b225e5f20
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-07-04 09:02:44 +0200

    Add limitation to pages, you cannot go further from the lowest and higher index

--- src/main/resources/static/js/listUser.js
+++ src/main/resources/static/js/listUser.js
@@ -16,9 +16,13 @@ function loadNextPage() {
         }
         })
         .then(function(data) {
-        var tableContent = extractTableContent(data);
-        console.log(tableContent);
-        document.getElementById("users").innerHTML = tableContent;
+            if(checkIfTableIsEmpty(data) === true){
+                alert("There's no more users");
+                currentPage--;
+            }else{
+                var tableContent = extractTableContent(data);
+                document.getElementById("users").innerHTML = tableContent;
+            }
         })
         .catch(function(error) {
         console.error(error);
@@ -28,7 +32,7 @@ function loadNextPage() {
 function loadPreviousPage() {    
     
     if(currentPage <= 0){
-        alert ("No hay páginas previas");
+        alert ("There's no previous users");
     }else{
         currentPage--;
         fetch('/userActions/listUser?page=' + currentPage + '&pageSize=' + pageSize)
@@ -50,13 +54,28 @@ function loadPreviousPage() {
 }
 
 
-  function extractTableContent(data) {
-    // Crear un nuevo elemento HTML para analizar el código
+function extractTableContent(data) {
+// Crear un nuevo elemento HTML para analizar el código
     var parser = new DOMParser();
     var htmlDoc = parser.parseFromString(data, 'text/html');
-  
+
     // Seleccionar solo el contenido de la tabla
     var tableContent = htmlDoc.querySelector('table').outerHTML;
-  
+
     return tableContent;
-  }
+}
+
+function checkIfTableIsEmpty(data) {
+// Crear un nuevo elemento HTML para analizar el código
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(data, 'text/html');
+
+    // Seleccionar solo el contenido de la tabla
+    var tableContent = htmlDoc.querySelector('tbody').innerHTML;
+    console.log(tableContent)
+    if(/^\s*$/.test(tableContent)){
+        return true;
+    }else{
+        return false;
+    }
+}
