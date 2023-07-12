8723df6d67b2b8e90db6f4fb7f4c5ef240dc2645
Pablo Ruiz Abellán <pabloruiz16@hotmail.com>
2023-07-12 22:40:04 +0200

    Pending to resolve AJAX issue and visual of the spinner

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,14 +1,35 @@
 //Ajax for buttons
 function ajaxB(element){
-    event.preventDefault();
-      var url = element.getAttribute('href');
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.onload = function() {
+    // event.preventDefault();
+    //   var url = element.getAttribute('href');
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.open('GET', url, true);
+    //   xhr.onload = function() {
+    //     document.getElementById('container').innerHTML = xhr.responseText;
+    //   };
+    //   xhr.send();
+  
+  let spinner = document.getElementById('mainSpinner');
+  var url = element.getAttribute('href');
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+
+  // Muestra el spinner antes de enviar la petición
+  spinner.style.display = 'flex';
+
+  xhr.onload = function() {
         document.getElementById('container').innerHTML = xhr.responseText;
-      };
-      xhr.send();
-}
+    
+
+      // Oculta el spinner al completarse la petición
+      spinner.style.display = 'none';
+    }
+  xhr.send();
+    
+};
+
+  
 
 
 //Ajax for functions
