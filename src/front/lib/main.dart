3efbc58b3aa5a1c685329051e79c304dcb992ef2
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-08-23 14:54:49 +0200

    Add html dependecy

--- src/front/lib/main.dart
+++ src/front/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Miguel Angel salta este numero de veces:'),
     );
   }
 }
