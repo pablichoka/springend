4c85d24747c81148825755ad99013c29c51c13f7
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-09-18 16:30:00 +0200

    Script now run boths servers. Pending add pause until backend compilation finishes.

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 @echo off
-start wt --title "SERVERS" ^
+start wt --title "COMPILER" ^
     -d .\ powershell "mvn package -DskipTests" ^
-    ; split-pane -d .\target powershell "java '-Dspring.data.mongodb.uri=%KCAL_MONGO_REMOTE%' -jar .\tool-0.0.1-SNAPSHOT.jar"
+    ; new-tab --title "FRONTEND" ^
+    -d .\src\front powershell "flutter run" ^
+    ; split-pane --title "BACKEND" ^
+    -d .\target powershell "java '-Dspring.data.mongodb.uri=%KCAL_MONGO_REMOTE%' -jar .\tool-0.0.1-SNAPSHOT.jar"
