603b362de9fe0b26266142c0a0421975f423791d
Pablo Ruiz Abellan <pablo.ruiz-abellan@capgemini.com>
2023-09-21 09:34:47 +0200

    Optimize scripts

--- scripts/jwtTokenGen.ps1
+++ scripts/jwtTokenGen.ps1
@@ -1,5 +1,9 @@
+param (
+    [string]$choice = ""
+)
+
 # Generate a random byte array as a secret
-$secretBytes = [byte[]]::new(32)  # Adjust the length as needed
+$secretBytes = [byte[]]::new(32)
 [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($secretBytes)
 
 # Encode the secret in base64 format
--- scripts/run.bat
+++ scripts/run.bat
@@ -2,7 +2,7 @@ start wt --title "COMPILER" -d ..\ powershell -Command "mvn package -DskipTests"
 powershell -ExecutionPolicy Bypass -File "jwtTokenGen.ps1" -choice "y"
 
 
-ping 127.0.0.1 -n 15 > nul
+ping 127.0.0.1 -n 10 > nul
 
-start wt --title "FRONTEND" -d ..\..\kCalControl_flutter powershell -noexit -Command "flutter run" ^
+start wt --title "FRONTEND" -d ..\..\kCalControl_flutter powershell -noexit -Command "flutter run -d chrome --verbose" ^
 ; split-pane -H --title "BACKEND" -d ..\target powershell -noexit -Command "java '-Djwt.base64secret=%JWT_TOKEN%' '-Dspring.data.mongodb.uri=%KCAL_MONGO_REMOTE%' '-Dserver.ssl.key-store-password=%SSL_KEY%' -jar .\tool-0.0.1-SNAPSHOT.jar"
