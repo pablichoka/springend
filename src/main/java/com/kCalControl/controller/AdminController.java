e225234c71581ed2dd0cdb86711b2aaf07cc0eb6
pablichoka <pabloruiz16@hotmail.com>
2023-09-27 18:07:13 +0200

    Recoding

--- src/main/java/com/kCalControl/controller/AdminController.java
+++ src/main/java/com/kCalControl/controller/AdminController.java
@@ -1,2 +1,35 @@
-package com.kCalControl.controller;public interface AdminController {
+package com.kCalControl.controller;
+
+import com.kCalControl.dto.creation.NewUserDTO;
+import com.kCalControl.dto.search.SearchParamsDTO;
+import com.kCalControl.model.Role;
+import jakarta.annotation.security.RolesAllowed;
+import org.bson.types.ObjectId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static com.kCalControl.model.Role.ADMIN_ID;
+
+
+@RestController
+@RequestMapping("api")
+@RolesAllowed("ADMIN")
+public interface AdminController {
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("admin/addUser")
+    ResponseEntity<Void> createAdminUser(@RequestBody NewUserDTO dto);
+
+    //TODO re-evaluate pagination method
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("admin/listUser")
+    String getUsersList(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int pageSize); //these values have lower priority than the JS ones
+
+    //TODO fix pagination: page entity adds the search result to the existing pageable
+    //TODO check parameters of JS functions passed to the server. Probably I have to include searchParams info into requests
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("admin/listUser")
+    String searchUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int pageSize,
+                       SearchParamsDTO dto);
 }
--- src/main/java/com/kCalControl/controller/impl/AdminControllerImpl.java
+++ src/main/java/com/kCalControl/controller/impl/AdminControllerImpl.java
@@ -1,2 +1,51 @@
-package com.kCalControl.controller.impl;public class AdminControllerImpl {
+package com.kCalControl.controller.impl;
+
+import com.kCalControl.controller.AdminController;
+import com.kCalControl.dto.creation.NewUserDTO;
+import com.kCalControl.dto.search.SearchParamsDTO;
+import com.kCalControl.model.UserDB;
+import com.kCalControl.repository.AssetsRepository;
+import com.kCalControl.repository.BMDataRepository;
+import com.kCalControl.repository.UserDBRepository;
+import com.kCalControl.service.UserDBService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.Store;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class AdminControllerImpl implements AdminController {
+
+    @Autowired
+    UserDBRepository userDBRepository;
+    @Autowired
+    BMDataRepository bmDataRepository;
+    @Autowired
+    AssetsRepository assetsRepository;
+    @Autowired
+    UserDBService userDBService;
+
+    @Override
+    public ResponseEntity<Void> createAdminUser(NewUserDTO dto) {
+        UserDB newUserDB = userDBService.newAdminUser(dto);
+        bmDataRepository.save(newUserDB.getBmData());
+        assetsRepository.save(newUserDB.getAssets());
+        userDBRepository.save(newUserDB);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    public String getUsersList(int page, int pageSize) {
+        Page<UserDB> usersList = userDBService.getUsers(page, pageSize);
+        return "/auth/admin/listUser";
+    }
+
+    @Override
+    public String searchUsers(int page, int pageSize, SearchParamsDTO dto) {
+        Page<UserDB> userSearchList = userDBService.getUsersFromSearch(page, pageSize, dto.getQuery(), dto.getFilter(), dto.getSort());
+        return "/auth/admin/listUser";
+    }
 }
