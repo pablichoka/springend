972aeedf559bf44fec8db1aa1aee2a8acfbd2d7e
Pablo Ruiz Abellán <pabloruiz16@hotmail.com>
2023-10-09 20:10:55 +0200

    Improve coding

--- src/main/java/com/kCalControl/config/Checker.java
+++ src/main/java/com/kCalControl/config/Checker.java
@@ -3,28 +3,39 @@ package com.kCalControl.config;
 import com.kCalControl.repository.UserDBRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 
+@Service
 public class Checker {
 
     @Autowired
     UserDBRepository userDBRepository;
 
-    public boolean checkRoleAdminByPrincipal(Principal principal){
-        return userDBRepository.findByUsername(principal.getName()).get().getRoleName().equals("ADMIN");
+    public boolean checkRoleAdminByPrincipal(Principal principal) {
+        return userDBRepository.findByUsername(principal.getName())
+                .map(user -> user.getRoleName().equals("ADMIN"))
+                .orElse(false);
     }
-    public boolean checkRoleAdminById(ObjectId id){
-        return userDBRepository.findById(id).get().getRoleName().equals("ADMIN");
+
+    public boolean checkRoleAdminById(ObjectId id) {
+        return userDBRepository.findById(id)
+                .map(user -> user.getRoleName().equals("ADMIN"))
+                .orElse(false);
     }
-    public boolean checkUserExistsByPrincipal(Principal principal){
+
+    public boolean checkUserExistsByPrincipal(Principal principal) {
         return userDBRepository.findByUsername(principal.getName()).isPresent();
     }
-    public boolean checkUserExistsById(ObjectId id){
+
+    public boolean checkUserExistsById(ObjectId id) {
         return userDBRepository.findById(id).isPresent();
     }
-    public boolean checkSameUser(Principal principal, ObjectId id){
-        return userDBRepository.findById(new ObjectId(principal.getName())).get().getId().equals(id);
+
+    public boolean checkSameUser(ObjectId id, Principal principal) {
+        return userDBRepository.findById(new ObjectId(principal.getName()))
+                .map(user -> user.getId().equals(id))
+                .orElse(false);
     }
 }
