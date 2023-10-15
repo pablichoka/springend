a0bbaf162d8106a93530d057fb0b4d8cfd79d69e
pablichoka <pabloruiz16@hotmail.com>
2023-10-15 17:32:20 +0200

    Add scheduled method to clean redis db of expired tokens

--- src/main/java/com/kCalControl/config/TokenRevocationService.java
+++ src/main/java/com/kCalControl/config/TokenRevocationService.java
@@ -1,14 +1,21 @@
 package com.kCalControl.config;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class TokenRevocationService {
 
     @Autowired
     StringRedisTemplate redisTemplate;
+
+    @Autowired
+    TokenManager tokenManager;
 
     private static final String REVOKED_TOKENS_KEY = "revoked_tokens";
 
@@ -18,5 +25,20 @@ public class TokenRevocationService {
 
     public boolean isTokenRevoked(String token) {
         return Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(REVOKED_TOKENS_KEY, token));
+    }
+
+    @Scheduled(fixedRate = 60000L * 60000L) // Check every 3600 seconds
+    public void cleanupExpiredTokens() {
+        Set<String> tokens = redisTemplate.opsForSet().members(REVOKED_TOKENS_KEY);
+        if (tokens == null) {
+            return;
+        }
+        for (String token : tokens) {
+            try {
+                tokenManager.validateClaims(tokenManager.parseToken(token));
+            } catch (ExpiredJwtException e) {
+                redisTemplate.opsForSet().remove(REVOKED_TOKENS_KEY, token);
+            }
+        }
     }
 }
