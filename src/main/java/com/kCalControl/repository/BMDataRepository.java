f732e4e65d832f9e234ab41a757a8880164e2edf
pablichoka <pabloruiz16@hotmail.com>
2023-11-14 23:04:21 +0100

    Query by usserAssoc

--- src/main/java/com/kCalControl/repository/BMDataRepository.java
+++ src/main/java/com/kCalControl/repository/BMDataRepository.java
@@ -1,14 +1,13 @@
 package com.kCalControl.repository;
 
 import com.kCalControl.model.BMData;
-import com.kCalControl.model.UserDB;
-import org.bson.types.ObjectId;
+import com.kCalControl.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BMDataRepository extends CrudRepository<BMData, Integer> {
-    BMData findByUserAssoc(UserDB userAssoc);
+    BMData findByUserAssoc(User userAssoc);
     BMData findByUserAssoc_Id(Integer id);
 
 }
